d5629eff4d93a56198bd4ddc06f876579d7c5592
Tom Powell <tom@powell.io>
2023-01-06 22:46:08 -0800

    add indexes, add official column

--- schema.sql
+++ schema.sql
@@ -7,7 +7,12 @@ CREATE TABLE IF NOT EXISTS stats (
     carrier_id character varying,
     submit_time timestamp without time zone DEFAULT now(),
     version character varying GENERATED ALWAYS AS ("substring"((version_raw)::text, '^\d\d\.\d'::text)) STORED,
+    official boolean GENERATED ALWAYS AS (version_raw::text ~ '\d\d\.\d-\d{8}-NIGHTLY-.*'::text) STORED,
 
     CONSTRAINT stats_pkey PRIMARY KEY (device_id)
 );
 
+CREATE INDEX stats_model ON stats(model);
+CREATE INDEX stats_version ON stats(version);
+CREATE INDEX stats_country ON stats(country);
+CREATE INDEX stats_carrier ON stats(carrier);
