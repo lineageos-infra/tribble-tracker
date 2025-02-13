f6b52c536ae206926424d35b8c8d4c7fcc3a4fc8
LuK1337 <priv.luk@gmail.com>
2025-02-13 22:41:55 +0100

    Add "public, " to Cache-Control header
    ...otherwise the result is deemed DYNAMIC and therefore uncachable.

--- main.go
+++ main.go
@@ -289,7 +289,7 @@ func main() {
 			return
 		}
 		data.Total = printer.Sprintf("%v", number.Decimal(total, number.Precision(3)))
-		w.Header().Add("Cache-Control", "max-age=60")
+		w.Header().Add("Cache-Control", "public, max-age=60")
 		err = tmpl.Execute(w, data)
 		if err != nil {
 			log := httplog.LogEntry(r.Context())
@@ -388,7 +388,7 @@ func main() {
 				data.Thing = name
 			}
 		}
-		w.Header().Add("Cache-Control", "max-age=60")
+		w.Header().Add("Cache-Control", "public, max-age=60")
 		err = tmpl.Execute(w, data)
 		if err != nil {
 			log := httplog.LogEntry(r.Context())
