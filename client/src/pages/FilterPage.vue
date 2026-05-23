5b9b0c53264bba70dfb9cc2e6a2c988c1e38bff2
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-05-23 22:30:29 +0200

    client: Drop primaryFilter logic

--- client/src/pages/FilterPage.vue
+++ client/src/pages/FilterPage.vue
@@ -8,18 +8,37 @@ SPDX-License-Identifier: Apache-2.0
 import FilterChip from '@/components/FilterChip.vue'
 import StatCard from '@/components/StatCard.vue'
 import { useStats } from '@/composables/useStats'
+import type { FilterColumn } from '@/api/types'
 import { filtersFromRoute, routeForClearingFilter } from '@/utils/filters'
 import { formatNumber } from '@/utils/format'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
+const CARDS: ReadonlyArray<{ column: FilterColumn; title: string }> = [
+  { column: 'model', title: 'Top Devices' },
+  { column: 'country', title: 'Top Countries' },
+  { column: 'version', title: 'Top Versions' },
+  { column: 'carrier', title: 'Top Carriers' },
+]
+
+const GRID_COLS = [
+  '',
+  'md:grid-cols-1',
+  'md:grid-cols-2',
+  'md:grid-cols-2 xl:grid-cols-3',
+  'md:grid-cols-2 xl:grid-cols-4',
+]
+
 const route = useRoute()
 const filters = computed(() => filtersFromRoute(route))
-const primaryFilter = computed(() => filters.value[0])
+const { data, loading, error } = useStats(() => filters.value.length ? filters.value : null)
 
-const { data, loading, error } = useStats(() => {
-  if (!filters.value.length) return null
-  return filters.value
+const visibleCards = computed(() => {
+  if (!data.value) return []
+  const filtered = new Set(filters.value.map((f) => f.column))
+  return CARDS
+    .filter(({ column }) => !filtered.has(column) && Object.keys(data.value![column]).length)
+    .map(({ column, title }) => ({ column, title, entries: data.value![column] }))
 })
 </script>
 
@@ -57,30 +76,13 @@ const { data, loading, error } = useStats(() => {
       Loading…
     </p>
 
-    <div v-else-if="data" class="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+    <div v-else-if="data" class="grid gap-4" :class="GRID_COLS[visibleCards.length]">
       <StatCard
-        v-if="primaryFilter.column !== 'model' && Object.keys(data.model).length"
-        title="Top Devices"
-        column="model"
-        :entries="data.model"
-      />
-      <StatCard
-        v-if="primaryFilter.column !== 'country' && Object.keys(data.country).length"
-        title="Top Countries"
-        column="country"
-        :entries="data.country"
-      />
-      <StatCard
-        v-if="primaryFilter.column !== 'version' && Object.keys(data.version).length"
-        title="Top Versions"
-        column="version"
-        :entries="data.version"
-      />
-      <StatCard
-        v-if="primaryFilter.column !== 'carrier' && data.carrier && Object.keys(data.carrier).length"
-        title="Top Carriers"
-        column="carrier"
-        :entries="data.carrier"
+        v-for="card in visibleCards"
+        :key="card.column"
+        :title="card.title"
+        :column="card.column"
+        :entries="card.entries"
       />
     </div>
   </div>
